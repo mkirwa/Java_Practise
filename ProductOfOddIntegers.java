98e04583c5da310eb98bbfaab4fc55e31d5dc4d2
mkirwa <mahlonkibiwott@gmail.com>
2016-10-31 02:11:42 -0400

    Add files via upload

---
+++ ProductOfOddIntegers.java
@@ -0,0 +1,39 @@
+/////////////////////////////////
+//////Calculating odd integers from 1 to 15
+
+import java.util.Scanner;
+
+public class ProductOfOddIntegers
+
+{
+
+
+
+		public static void main(String[]args)
+
+		{
+
+			int product=1;
+			
+			
+			for(int i = 1; i<=15 ; i=i+2)
+			{
+
+				product = product * i;
+
+			}
+
+			System.out.println(product);
+
+			//storing the test cases in an array
+			//finding the product of odd numbers in the test cases
+			//printing the output
+
+
+		}
+
+
+
+
+
+}
